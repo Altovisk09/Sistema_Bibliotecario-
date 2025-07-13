fc1793f45c5691189695ca3a6ed80696308d24f6
Altovisk09 <ericaltao13@gmail.com>
2025-07-13 10:50:58 -0300

    feat(geral):Processo de implementação de dtos em andamento

--- src/main/java/com/library/controller/BookController.java
+++ src/main/java/com/library/controller/BookController.java
@@ -1,10 +1,28 @@
 package com.library.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.library.model.Book;
+import com.library.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/book")
 public class BookController {
 
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+    @GetMapping
+    public List<Book> getAll(){return bookService.getAll();}
+
+    @PostMapping
+    public ResponseEntity<String> registerBook(@RequestBody Book book){
+        bookService.save(book);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Livro cadastrado com sucesso");
+    }
 }
--- src/main/java/com/library/service/BookService.java
+++ src/main/java/com/library/service/BookService.java
@@ -1,4 +1,20 @@
 package com.library.service;
 
+import com.library.repository.BookRepository;
+import org.springframework.stereotype.Service;
+import com.library.model.Book;
+
+import java.util.List;
+
+@Service
 public class BookService {
+    private BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    //Listar
+    public List<Book> getAll(){return bookRepository.findAll();}
+    //Criar
+    public Book save(Book book) {return bookRepository.save(book);}
 }
