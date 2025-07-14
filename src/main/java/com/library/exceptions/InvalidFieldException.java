d9813c4e06bacaa7bef983dc2ce9e161903507f1
Altovisk09 <ericaltao13@gmail.com>
2025-07-14 19:49:01 -0300

    refactor(exceptions):Alteração de extends Exception para RunTimeException.

--- src/main/java/com/library/exceptions/InvalidFieldException.java
+++ src/main/java/com/library/exceptions/InvalidFieldException.java
@@ -1,6 +1,6 @@
 package com.library.exceptions;
 
-public class InvalidFieldException extends Exception {
+public class InvalidFieldException extends RuntimeException {
     public InvalidFieldException(String message) {
         super(message);
     }
