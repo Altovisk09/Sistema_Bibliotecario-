d629cd9bc10d6cdf25e79369876d32ff50386ebd
Altovisk09 <ericaltao13@gmail.com>
2025-07-16 18:50:38 -0300

    fix(models):Correção de escopo em Book

--- src/main/java/com/library/model/Book.java
+++ src/main/java/com/library/model/Book.java
@@ -3,12 +3,11 @@ package com.library.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
+@Getter
+@Setter
 @Entity
 @Table(name = "book_table")
 public class Book {
-    @Getter
-    @Setter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
