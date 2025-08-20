bc97028eb5d93cd78c09877507d3984736da3d14
Altovisk09 <ericaltao13@gmail.com>
2025-08-20 17:07:27 -0300

    refactor(geral): dependências atualizadas

--- src/main/java/com/library/SpringLibraryApplication.java
+++ src/main/java/com/library/SpringLibraryApplication.java
@@ -1,6 +1,5 @@
 package com.library;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,14 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringLibraryApplication {
 
 	public static void main(String[] args) {
-		// carrega o .env da raiz do projeto
-		Dotenv dotenv = Dotenv.load();
-
-		// injeta cada variável do .env como System property
-		dotenv.entries().forEach(entry ->
-				System.setProperty(entry.getKey(), entry.getValue())
-		);
-
 		SpringApplication.run(SpringLibraryApplication.class, args);
 	}
 
